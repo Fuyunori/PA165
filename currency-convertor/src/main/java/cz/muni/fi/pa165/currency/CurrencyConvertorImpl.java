5da951eaa7ee3f5f8468b1fb18de8aaa74970952
Xuan Linh Phamová <xphamov2@fi.muni.cz>
2021-03-11 18:32:32 +0100

    added logging - task 06 almost completed, not tested yet

--- currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
@@ -1,5 +1,8 @@
 package cz.muni.fi.pa165.currency;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Currency;
@@ -13,7 +16,7 @@ import java.util.Currency;
 public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     private final ExchangeRateTable exchangeRateTable;
-    //private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
 
     public CurrencyConvertorImpl(ExchangeRateTable exchangeRateTable) {
         this.exchangeRateTable = exchangeRateTable;
@@ -21,11 +24,14 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     @Override
     public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
+        logger.trace("Convert method called with this parameter {}", exchangeRateTable);
         if(sourceCurrency == null || targetCurrency == null || sourceAmount == null){
+            logger.warn("At least one of the arguments of the convert method is null.");
             throw new IllegalArgumentException();
         }
 
         if(sourceCurrency.equals(targetCurrency)){
+            logger.trace("Convert method successfully finished with two same currency as arguments.");
             return convertToTwoDecimalPointsUsingHalfEven(sourceAmount);
         }
 
@@ -33,12 +39,15 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
             BigDecimal exchangeRate = exchangeRateTable.getExchangeRate(sourceCurrency,targetCurrency);
 
             if(exchangeRate == null){
+                logger.warn("Exchange rate returned by {} is null.", exchangeRateTable);
                 throw new UnknownExchangeRateException("The currency is unknown");
             }
 
             BigDecimal result = sourceAmount.multiply(exchangeRate);
+            logger.trace("Convert method successfully finished.");
             return convertToTwoDecimalPointsUsingHalfEven(result);
         } catch (ExternalServiceFailureException esfe){
+            logger.error("Exchange rate returned by {} couldn't be retrieved.", exchangeRateTable);
             throw new UnknownExchangeRateException("The exchange rate couldn't be retrieved.");
         }
     }
