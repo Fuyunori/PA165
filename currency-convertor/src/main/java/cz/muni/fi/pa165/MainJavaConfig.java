bd2ce65c8adbd063b9a591aa0e6678b7691a3311
Xuan Linh Phamová <xphamov2@fi.muni.cz>
2021-03-16 19:06:01 +0100

    task 07 completed

---
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/MainJavaConfig.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.pa165;
+
+import cz.muni.fi.pa165.currency.CurrencyConvertor;
+import cz.muni.fi.pa165.currency.CurrencyConvertorImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public class MainJavaConfig {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(JavaConfig.class);
+        CurrencyConvertor currencyConvertor = applicationContext.getBean(CurrencyConvertorImpl.class);
+        System.out.println(currencyConvertor.convert(Currency.getInstance("EUR"), Currency.getInstance("CZK"), BigDecimal.ONE));
+    }
+}
